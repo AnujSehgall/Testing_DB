3db6be3c8dfd33123b3ab1669e7b54a4c969432e
AnujSehgall <anujsehgal.04@gmail.com>
2017-04-09 21:11:37 +0530

    Login

---
+++ app/src/main/java/com/anuj/testing_db/BackgroundWorker.java
@@ -0,0 +1,88 @@
+package com.anuj.testing_db;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by anuj on 9/4/17.
+ */
+
+public class BackgroundWorker extends AsyncTask<String, Void,String> {
+
+    Context cntxt;
+    AlertDialog alertDialog;
+    BackgroundWorker (Context ctx){
+        cntxt = ctx;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String login_url = "http://192.168.1.102/login.php";
+        if(type.equals("login")){
+            try {
+                String user_name = params[1];
+                String pass_word = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"+URLEncoder.encode("pass_word","UTF-8")+"="+URLEncoder.encode(pass_word,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="",line="";
+                while ((line = bufferedReader.readLine()) != null){
+                        result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(cntxt).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+}
--- app/src/main/java/com/anuj/testing_db/MainActivity.java
+++ app/src/main/java/com/anuj/testing_db/MainActivity.java
@@ -2,12 +2,34 @@ package com.anuj.testing_db;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
+    EditText name,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login =(Button) findViewById(R.id.button);
+        name = (EditText) findViewById(R.id.editText);
+        pass = (EditText) findViewById(R.id.editText3);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = name.getText().toString();
+                String password = pass.getText().toString();
+                String type = "login";
+                BackgroundWorker backgroundWorker = new BackgroundWorker(MainActivity.this);
+                backgroundWorker.execute(type,username,password);
+
+
+            }
+        });
     }
 }
